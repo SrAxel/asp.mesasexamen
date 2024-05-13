1c43f0171b096eb9035ddf04a365d61f8bab5c46
SrAxel <Thesense@outlook.es>
2024-05-13 15:25:49 -0300

    Inicio de Proyecto

---
+++ Pages/35Aniversario.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MesasDeExamen.Pages
+{
+    public class _35AniversarioModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
